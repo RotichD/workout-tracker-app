e4367244243b1666bf11a9edffdeb0fccc0cdd60
Dylan Rotich <65515418+RotichD@users.noreply.github.com>
2023-03-03 19:04:52 -0800

    * Styled Tab Bar navigation * Styled Welcome on HomeScreen

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -3,15 +3,31 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../src/screens/HomeScreen';
 import ExercisesScreen from '../src/screens/ExercisesScreen';
 import ProfileScreen from '../src/screens/ProfileScreen';
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 const AppStack = () => {
   return (
     <Tab.Navigator
-      screenOptions={{
+      screenOptions={({route}) => ({
         headerShown: false,
-      }}
+        tabBarActiveTintColor: '#f59e0b',
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = 'ios-home';
+            return <Ionicons name={iconName} size={size} color={color} />;
+          } else if (route.name === 'Profile') {
+            iconName = 'ios-person';
+            return <Ionicons name={iconName} size={size} color={color} />;
+          } else {
+            return <MaterialCommunityIcons name="weight-lifter" size={24} color={color} />;
+          }
+          
+        },
+      })}
     >
       <Tab.Screen name='Home' component={HomeScreen} />
       <Tab.Screen name='Exercises' component={ExercisesScreen} />
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { auth } from "../../firebase";
 
 const HomeScreen = () => {
+  const displayName = auth.currentUser.displayName;
   return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
+    <SafeAreaView className="flex-1 pt-0">
+      <Text className="text-3xl text-center font-bold text-gray-900 mt-5 shadow-amber-400">
+        Welcome
+      </Text>
+      <Text className="text-center font-bold text-gray-500 shadow-amber-400">
+        {displayName}
+      </Text>
+    </SafeAreaView>
   );
 };
 
